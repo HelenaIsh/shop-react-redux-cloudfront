9d93e97d67c2aec830b1bc6f0d7bba41bc287b93
elena_ishmatova <elena_ishmatova@epam.com>
2026-05-14 17:11:01 +0200

    feat: add auth token for file uploading

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -29,11 +29,15 @@ export default function CSVFileImport({ url, title }: CSVFileImportProps) {
     if (!file) return;
 
     // Get the presigned URL
+    const authorization_token = localStorage.getItem("authorization_token");
     const response = await axios({
       method: "GET",
       url,
       params: {
         name: encodeURIComponent(file.name),
+      },
+      headers: {
+        Authorization: `Basic ${authorization_token}`,
       },
     });
     console.log("File to upload: ", file.name);
